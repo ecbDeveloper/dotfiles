5d8126e1eb603792dd0304cfdf67ba8203b6723f
ecbDeveloper <developer.ecb@gmail.com>
2025-08-29 12:44:46 -0300

    .

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      hidden = true,
+      ignored = true,
+      sources = {
+        explorer = {
+          layout = {
+            layout = {
+              position = "right",
+            },
+          },
+        },
+      },
+    },
+  },
+}
